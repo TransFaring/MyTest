10c481628018ef4f04292b963500e6f1a908ec3d
ZhanXiang <bjzhanxiang@corp.netease.com>
2016-12-01 22:19:07 +0800

    test

--- src/test2/Test.java
+++ src/test2/Test.java
@@ -1,47 +1,42 @@
 package test2;
 
-
 public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int[] arr=new int[]{6087,5173,1358,3825,2531};
-		test(arr);
-	
+		String[] arr=new String[]{"6087","5173","1358","3825","2531"};
+		test(arr);	
 	}
 	
-	private static void test(int[] testArray){
+	private static void test(String[] array){
 		
-		for(int i=1000;i<10000;i++){
-			int[] a=getArr(i);
-			int count=0;
-			for(int j=0;j<testArray.length;j++){
-				int[] b=getArr(testArray[j]);
+		for(int i=0;i<10000;i++){
+			int a[];
+			int count=0;		
+			String m=String.format("%04d", i);
+			a=getArr(m);
+			for(int j=0;j<array.length;j++){
+				int[] b=getArr(array[j]);
 				if(isSuccess(a, b)){
 					count++;
-				}
-				if(j==testArray.length-1&&count==5){
-					System.out.println(i);
-				}				
-			}			
+				}			
+			}
+			if(count==5)
+				System.out.println(i);
 		}
-		System.out.println("运行结束");
-		
+		System.out.println("运行结束");		
 		
 	}
-	private static int[] getArr(int num) {
-		int[] arr=new int[4];
-		arr[0]=num/1000;
-		arr[1]=num/100%10;
-		arr[2]=num/10%10;
-		arr[3]=num%10;
-		return arr;
-		
+	private static int[] getArr(String s) {
+		int[] arr=new int[s.length()];		
+		for (int i = 0; i < s.length(); i++) {  
+			 char c = s.charAt(i);  
+			 arr[i] = Character.getNumericValue(c);  
+		}  
+		return arr;		
 	}
 	private static boolean isSuccess(int[] a,int[] b) {
 		int count=0;
-		boolean[][] bp=new boolean[4][4];
-		
 		for(int i=0;i<a.length;i++){
 			
 			for(int j=0;j<b.length;j++){
@@ -49,16 +44,13 @@ public class Test {
 					return false;
 				}
 				if(a[i]==b[j]&i!=j){
-					bp[i][j]=true;	
 					count++;
 				}			
 			}
 		}
-
 		if(count==2){
 			return true;
 		}		
 		return false;
 	}
-
 }
